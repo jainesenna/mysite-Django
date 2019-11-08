cb0f97408aac0967a5aedabfe2d2fc1c160a3ca2
Jaine Senna <jainesantossenna191@gmail.com>
2019-11-08 09:09:51 -0300

    Atualizando o models.py

--- polls/models.py
+++ polls/models.py
@@ -1,26 +1,7 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-class Question(models.Model):
-# ...
-    def __str__(self):
-        return self.question_text
-
-class Choice(models.Model):
-    # ...
-    def __str__(self):
-        return self.choice_text
 
 class Question(models.Model):
     # ...
