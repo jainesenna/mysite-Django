d617a66e240b804381e9a2b9369e0b3ee2b663ed
Jaine Senna <jainesantossenna191@gmail.com>
2019-11-08 08:29:38 -0300

    Playing with the API

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Bom dia, que tal ouvir Vor Í Vaglaskógi de Kaleo")
